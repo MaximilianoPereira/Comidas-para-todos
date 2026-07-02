70d3918f4ef665fcf4ff67b022fc46f00145da52
ccalder0n <calderon.ca93@gmail.com>
2026-07-01 21:13:36 -0300

    feat: preparacion para incorporacion de js + bootstrap

---
+++ js/main.js
@@ -0,0 +1,5 @@
+// Modo oscuro
+const modoActivo = document.body.classList.contains("modo-oscuro")
+const btnModo = document.getElementById("btn-modo")
+btnModo.addEventListener("click", () => { document.body.classList.toggle("modo-oscuro") })
+
