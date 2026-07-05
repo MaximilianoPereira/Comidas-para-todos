57fffd4334f2374523833f93d4bfd5338b7fc872
ccalder0n <calderon.ca93@gmail.com>
2026-07-04 22:03:28 -0300

    feat: correccion navbar + bootstrap, funcional en moviles

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
 // Modo oscuro
-const modoActivo = document.body.classList.contains("modo-oscuro")
-const btnModo = document.getElementById("btn-modo")
-btnModo.addEventListener("click", () => { document.body.classList.toggle("modo-oscuro") })
+// const modoActivo = document.body.classList.contains("modo-oscuro")
+// const btnModo = document.getElementById("btn-modo")
+// btnModo.addEventListener("click", () => { document.body.classList.toggle("modo-oscuro") })
+
+// Efecto hamburguesa
+const navMenu = document.getElementById("navMenu")
+const btnHamb = document.getElementById("btnHamb")
+btnHamb.addEventListener("click", () => { navMenu.classList.toggle("show") })
 
